f53f1fe4eacccbbcbb68c5ade78e5967f20e8b05
Gilles Verstraeten <gilles.verstraeten@student.vives.be>
2019-05-01 19:06:36 +0200

    Database Scaffold

---
+++ ProjectTreinritten.Domain/Entities/Adults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectTreinritten.Domain.Entities
+{
+    public partial class Adults
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollDate { get; set; }
+        public int DepartmentId { get; set; }
+
+        public Departments Department { get; set; }
+    }
+}
---
+++ ProjectTreinritten.Domain/Entities/StudentsContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ProjectTreinritten.Domain.Entities
+{
+    public partial class StudentsContext : DbContext
+    {
+        public StudentsContext()
+        {
+        }
+
+        public StudentsContext(DbContextOptions<StudentsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Adults> Adults { get; set; }
+        public virtual DbSet<Departments> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-9K57NLQ\\SQL_VIVES;Database=Students;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Adults>(entity =>
+            {
+                entity.Property(e => e.EnrollDate).HasColumnType("datetime");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Department)
+                    .WithMany(p => p.Adults)
+                    .HasForeignKey(d => d.DepartmentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Students_Departments");
+            });
+
+            modelBuilder.Entity<Departments>(entity =>
+            {
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(40);
+            });
+        }
+    }
+}
