d3b41ec10da681ba66f34ff7119be05115454b58
Pieter Debaere <pieter.debaere@student.vives.be>
2019-05-01 16:44:13 +0200

    oude dingen toegevoegd

--- ProjectTreinritten/Controllers/TreinController.cs
+++ ProjectTreinritten/Controllers/TreinController.cs
@@ -1,14 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using Microsoft.AspNetCore.Mvc;
 
 namespace ProjectTreinritten.Controllers
 {
     public class TreinController : Controller
     {
+        //startpagina met linken naar alle functionaliteiten
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        //pagina met alle informatie over het project: in opdracht van, door wie gemaakt etc.
+        public IActionResult Over()
+        {
+            return View();
+        }
+
+        //pagina om een boeking te doen
+        public IActionResult Boeken()
+        {
+            return View();
+        }
+
+        //pagina die bevestiging van boeking toont
+        public IActionResult Bevestiging()
+        {
+            return View();
+        }
+
+        //pagina om bestelgeschiedenis te bekijken
+        public IActionResult Historiek()
+        {
+            return View();
+        }
+
+        //pagina om winkelkar te bekijken
+        public IActionResult Winkelkar()
+        {
+            return View();
+        }
+
+        //pagina om in te loggen
+        public IActionResult Inloggen()
+        {
+            return View();
+        }
+
+        //pagina voor account aan te maken
+        public IActionResult Registreer()
+        {
+            return View();
+        }
+
+        //404 pagina, deze pagina word getoond indien de pagina waarnaar men surft leeg is
+        public IActionResult Leeg()
+        {
+            return View();
+        }
+
+        //pagina waar men wachtwoord kan opvragen via email indien men deze vergeten is
+        public IActionResult Wachtwoord()
         {
             return View();
         }
--- ProjectTreinritten/Startup.cs
+++ ProjectTreinritten/Startup.cs
@@ -67,7 +67,7 @@ namespace ProjectTreinritten
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Trein}/{action=Index}/{id?}");
             });
         }
     }
